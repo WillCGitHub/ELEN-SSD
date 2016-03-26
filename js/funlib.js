e36507d0389438c1590881abd303f1f858e64078
Tianhua Fang <tianhuaf0526@gmail.com>
2016-03-26 01:38:09 -0400

    The copy function will put copy all the result now. Modified the html to add a hidden text area.

--- js/funlib.js
+++ js/funlib.js
@@ -16,3 +16,36 @@ function CopyToClip(){
 	output_Potential.select();
 	document.execCommand('copy');
 }
+
+function CopyToClipResult(){
+	var buffer="";
+
+	//read data
+	var Potential = document.getElementById("output_Potential");   //match the order of the UI
+	var CurrentDen_p = document.getElementById("output_currentDen_p");
+	var CurrentDen_n = document.getElementById("output_currentDen_n");
+	var Resistivity_p = document.getElementById("output_resistivity_p");
+	var Resistivity_n = document.getElementById("output_resistivity_n");
+	var DepRegion_p = document.getElementById("output_depletionRegion_p");
+	var DepRegion_n = document.getElementById("output_depletionRegion_n");
+	var DepRegion_d = document.getElementById("output_depletionRegion_d");
+	var Capacitance = document.getElementById("output_capacitance");
+	
+	
+	//put data into array
+	var tag=["Potential", "Ip","In","Rp","Rn","DR_p","DR_n","DR_d","Cap"];
+	var data=[Potential.value,CurrentDen_n.value,CurrentDen_p.value,Resistivity_n.value,Resistivity_p.value,
+			  DepRegion_p.value,DepRegion_n.value,DepRegion_d.value,Capacitance.value];
+	
+	//format array
+	for(i=0;i<9;i++){
+		buffer=buffer+tag[i]+" "+data[i]+"\n";
+	}
+
+	//put into text hidden field ta
+	document.getElementById('ta').value=buffer;
+
+	//copy to the clopboard
+	ta.select('AnsTextArea');
+	document.execCommand('copy');
+}
